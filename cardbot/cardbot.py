8050c37f83eaf7884255b5c36035ea2cca3f5113
Chombler <64819823+Chombler@users.noreply.github.com>
2020-09-13 17:39:44 -0400

    Update cardbot.py

--- cardbot/cardbot.py
+++ cardbot/cardbot.py
@@ -219,9 +219,12 @@ async def regularSearch(message):
 			response = pullHeroRecord(text)
 			print("Channel name: %s" % (message.channel.name))
 			print("Channel id: %s" % (message.channel.id))
-			if(message.channel.id == bot_spam_channel_id or message.channel.id == cardbot_bugs_report_channel_id):
-				await message.channel.send(response + "\n||Record generated in response to command: \{\{" + text + "\}\}||")
-			else:
+			try:
+				if(message.channel.id == bot_spam_channel_id or message.channel.id == cardbot_bugs_report_channel_id):
+					await message.channel.send(response + "\n||Record generated in response to command: \{\{" + text + "\}\}||")
+				else:
+					await message.channel.send(response)
+			except:
 				await message.channel.send(response)
 
 	if '[[' and ']]' in message.content:
@@ -234,9 +237,12 @@ async def regularSearch(message):
 			response = pullCardRecord(text)
 			print("Channel name: %s" % (message.channel.name))
 			print("Channel id: %s" % (message.channel.id))
-			if(message.channel.id == bot_spam_channel_id or message.channel.id == cardbot_bugs_report_channel_id):
-				await message.channel.send(response + "\n||Record generated in response to command: \{\{" + text + "\}\}||")
-			else:
+			try:
+				if(message.channel.id == bot_spam_channel_id or message.channel.id == cardbot_bugs_report_channel_id):
+					await message.channel.send(response + "\n||Record generated in response to command: \{\{" + text + "\}\}||")
+				else:
+					await message.channel.send(response)
+			except:
 				await message.channel.send(response)
 
 
