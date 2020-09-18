43ec5160baef8bb96e09dd8a52e24b38610d0cb0
Chombler <64819823+Chombler@users.noreply.github.com>
2020-09-18 10:41:54 -0400

    Update cardbot.py

--- cardbot/cardbot.py
+++ cardbot/cardbot.py
@@ -223,8 +223,8 @@ async def fuzzySearch(message):
 			response = pullFuzzyCardRecord(text)
 			try:
 				print("Channel name: %s" % (message.channel.name))
-				print("Channel id: %s" % (message.channel.name))
-				await message.channel.send("#%s" % (message.channel.id))
+				print("Channel id: %s" % (message.channel.id))
+				await message.channel.send("#%s" % (message.channel.name))
 				if(message.channel.id == bot_spam_channel_id or message.channel.id == cardbot_bugs_report_channel_id):
 					await message.channel.send(response + "\n||Record generated in response to command: \{\{" + text + "\}\}||")
 				else:
