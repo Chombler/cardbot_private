32d079b7cfeb901d1191f7abca05c3564d6f2096
Chombler <64819823+Chombler@users.noreply.github.com>
2020-09-30 11:04:29 -0400

    Update tempcode.py

--- cardbot/tempcode.py
+++ cardbot/tempcode.py
@@ -1,7 +1,7 @@
 
 from tables.tournament import timezone, participant, tournament, participant_to_tournament, tournament_participant_to_bans, tournament_participant_to_ign, matchup
 from constructorRows import timezone_rows
-from db_interactions_tournament import getParticipantInfo
+from db_interactions_tournaments import getParticipantInfo
 
 def handyman(complete_call):
 	print(getParticipantInfo("Chombler"))
