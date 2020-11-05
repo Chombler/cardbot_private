d497cd1552cad198af6f8826d9570f078f1d560c
Chombler <64819823+Chombler@users.noreply.github.com>
2020-11-05 02:22:57 -0600

    Update timer.py

--- cardbot/timer.py
+++ cardbot/timer.py
@@ -25,7 +25,7 @@ class Countdown:
 	def timeRemaining(self):
 		if(self.start_time is None):
 			return(None)
-		return(math.floor(self.limit - self.timePassed))
+		return(math.floor(self.limit - self.timePassed()))
 
 	def isFinished(self):
 		if(not self.hasStarted()):
