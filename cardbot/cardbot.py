b4003d789fdb16ca28c6e4e0021cbf703e5d842d
Chombler <64819823+Chombler@users.noreply.github.com>
2020-12-19 18:12:44 -0600

    Update cardbot.py

--- cardbot/cardbot.py
+++ cardbot/cardbot.py
@@ -100,8 +100,8 @@ async def on_message(message):
 
 @client.event
 async def on_reaction_add(reaction, user):
-	print(reaction)
-	print(reaction == ✅)
+	print(reaction.emoji)
+	print(reaction.emoji == '✅')
 
 async def fuzzySearch(message):
 	if '{{' and '}}' in message.content:
