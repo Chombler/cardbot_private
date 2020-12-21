c25c0bb7ce525c1e14e1f90638b339aeeeefb66f
Chombler <64819823+Chombler@users.noreply.github.com>
2020-12-20 23:32:31 -0600

    Update elo.py

--- cardbot/elo.py
+++ cardbot/elo.py
@@ -17,6 +17,8 @@ def getElo(name):
 
 		cursor.execute(select_query, (name,))
 
+		print(cursor.fetchall())
+
 		if(len(cursor.fetchall()) > 0):
 			elo = cursor.fetchall()[0][0]
 		else:
@@ -90,17 +92,11 @@ def calculateResults(winner, loser):
 	start_winner_elo = getElo(winner)
 	start_loser_elo = getElo(loser)
 
-	print(start_winner_elo)
-	print(start_loser_elo)
-
 	winner_expected = 1 / (1 + pow(10, (start_loser_elo - start_winner_elo) / 400))
 	loser_expected = 1 / (1 + pow(10, (start_winner_elo - start_loser_elo) / 400))
 
 	final_winner_elo = start_winner_elo + 30 * (1 - winner_expected)
 	final_loser_elo = start_loser_elo + 30 * (0 - loser_expected)
-
-	print(final_winner_elo)
-	print(final_loser_elo)
 
 	return([start_winner_elo, final_winner_elo, start_loser_elo, final_loser_elo])
 
@@ -108,5 +104,4 @@ def applyResults(winner, loser):
 	results = calculateResults(winner, loser)
 	updateElo(winner, results[1])
 	updateElo(loser, results[3])
-
 
