0391752967ef6320f2685cfb4cf979d810b3a3eb
Chombler <64819823+Chombler@users.noreply.github.com>
2021-05-20 23:10:37 -0500

    Update tempcode.py

--- cardbot/tempcode.py
+++ cardbot/tempcode.py
@@ -2,7 +2,7 @@ from elo import getElo
 from classes.fetch_query import test
 
 def handyman(complete):
-	print(getElo(445781406111760415))
+	print(getElo('Chombler', 445781406111760415))
 	print(test())
 	return("")
 
